4ebf9efc70ad2d8656fd34ef2635148c59b9e4dc
Katharine Hollars <85190553+shindokl90-lab@users.noreply.github.com>
2021-11-22 10:00:12 -0600

    Add files via upload

---
+++ ufo-level-1/static/js/app.js
@@ -0,0 +1,52 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// Select the table and table body 
+
+var table = d3.select("table");
+var tbody = d3.select("tbody");
+
+
+// Select the filter button 
+
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+// Activate the filter button click mechanism 
+button.on("click", runEnter);
+d3.select("body").on("keypress", function(){
+    if(d3.event.keycode===32 || d3.event.keyCode ===13){
+        runEnter();
+    }
+});
+
+// Build the table 
+
+buildtable(table);
+
+// Apply functionality to table 
+
+function buildtable(tdata){
+    tbody.html("");
+    tdata.forEach((ufoSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+        });
+    });
+}
+
+// Run the form 
+function runEnter(){
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
+
+    console.log(filteredData);
+    buildtable(filteredData);
+}
